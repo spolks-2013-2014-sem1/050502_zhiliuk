75aa085a23c3bece7438fdb2c54d7018a79ec2b6
Crashtown <crashtown.pal@gmail.com>
2013-11-21 02:22:05 +0300

    Ruler = 80

--- lab2/lab2.rb
+++ lab2/lab2.rb
@@ -1,16 +1,23 @@
 require 'socket'
 include Socket::Constants
-socket = Socket.new(AF_INET, SOCK_STREAM, 0)                     #create new server sock
-sockaddr = Socket.sockaddr_in(22328, 'localhost')                #=> "\x02\x00W8\x7F\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"
-socket.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR, true) #Set ::Option. for reusing addr
-socket.bind(sockaddr)                                            # Bind socket to addr
-socket.listen(5)                                                 #start listening
+# Create new server sock
+socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+# => "\x02\x00W8\x7F\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"
+sockaddr = Socket.sockaddr_in(22328, 'localhost')
+# Set ::Option. for reusing addr
+socket.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR, true)
+# Bind socket to addr
+socket.bind(sockaddr)
+# Start listening
+socket.listen(5)
 puts 'Listening...'
-client, client_addrinfo = socket.accept                          #on connect 
+# On connect 
+client, client_addrinfo = socket.accept
 begin
   client.puts Time.now.utc 
   puts "Client => '#{Time.now.utc}'"
-rescue Interrupt                                                 # ctrl+c interrupt catch (not working idkw)
+# ctrl+c interrupt catch (not working idkw)  
+rescue Interrupt
   puts "I refuse to be stopped!"
 ensure
   socket.close
