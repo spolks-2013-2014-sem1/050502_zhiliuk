8228db4dcb55a8c1e6b56abd8a90d8b3e14494c6
Crashtown <crashtown.pal@gmail.com>
2013-11-26 15:29:57 +0300

    Added options parser

--- lab2/lab2.rb
+++ lab2/lab2.rb
@@ -1,15 +1,43 @@
 require 'socket'
 include Socket::Constants
+require 'optparse'
+
+
+options = { port: nil, address: 'localhost' }
+
+parser = OptionParser.new do|opts|
+  opts.banner = "Usage: server.rb [options]"
+  opts.on('-p', '--port port', 'Port') do |port|
+    options[:port] = port;
+  end
+
+  opts.on('-a', '--address address', 'Address') do |address|
+    options[:address] = address;
+  end
+
+  opts.on('-h', '--help', 'Displays Help') do
+    puts opts
+    exit
+  end
+end
+
+parser.parse!
+
+if options[:port] == nil
+  print 'Enter Port: '
+  options[:port] = gets.chomp
+end
+
 # Create new server sock
 socket = Socket.new(AF_INET, SOCK_STREAM, 0)
 # => "\x02\x00W8\x7F\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00"
-sockaddr = Socket.sockaddr_in(22328, 'localhost')
+sockaddr = Socket.sockaddr_in(options[:port], options[:address])
 # Set ::Option. for reusing addr
 socket.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR, true)
 # Bind socket to addr
 socket.bind(sockaddr)
 # Start listening
-socket.listen(5)
+socket.listen(1)
 puts 'Listening...'
 # On connect 
 client, client_addrinfo = socket.accept
